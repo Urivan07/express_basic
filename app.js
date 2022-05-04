1ac6f7e09b711bfa6a1b3096f7f52598e3aef7ff
Urivan <urivan07@gmail.com>
2022-05-04 16:22:24 -0500

    Add - route /exporers/:explorer y recibiendo query params

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.get('/node', (req, res) => {
     res.send([explorer1, explorer2, explorer3])
 })
 
+// localhost:300/explorers/urivan
+app.get('/explorers/:explorer', (req, res) => {
+    console.log(req.params)
+    res.send(req.params)
+})
+
 app.listen(port, () => {
     console.log("Server Listo!")
 })
